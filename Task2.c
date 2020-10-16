faad00d15235f0c5d9364d3df1144d5420c68b54
whoyoude <62922857+whoyoude@users.noreply.github.com>
2020-10-15 23:39:30 -0400

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -17,11 +17,6 @@ unsigned int stop;
 unsigned int UartReady = 0;
 
 void GPIO_set();
-void readInput();
-//void USART1_IRQHandler();
-//void USART6_IRQHandler();
-void print_message();
-//void HAL_UART_RxCallBack(UART_HandleTypeDef *uart_init);
 
 // Main Execution Loop
 int main(void) {
@@ -39,9 +34,7 @@ int main(void) {
 }
 
 void GPIO_set(){
-//	HAL_Init();
-   // SystemClock_Config();
-
+     //Set the interrupt
     HAL_NVIC_SetPriority(USART1_IRQn,0,1);
     HAL_NVIC_SetPriority(USART6_IRQn,0,0);
     HAL_NVIC_EnableIRQ(USART1_IRQn);
@@ -49,7 +42,7 @@ void GPIO_set(){
 
     __HAL_RCC_USART6_CLK_ENABLE();
     __HAL_RCC_USART1_CLK_ENABLE();
-
+        //Configure USART6
 	uart_init6.Instance = USART6;
 	uart_init6.Init.BaudRate = 38400;
 	uart_init6.Init.WordLength = UART_WORDLENGTH_8B;
@@ -58,9 +51,8 @@ void GPIO_set(){
 	uart_init6.Init.Mode       = UART_MODE_TX_RX;
 	uart_init6.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
 	HAL_UART_Init(&uart_init6);
-	//HAL_UART_MspInit(&uart_init6);
-	//__HAL_UART_ENABLE_IT(&uart_init6, UART_IT_RXNE);
-
+	
+       //Configure USART1
 	uart_init1.Instance = USART1;
 	uart_init1.Init.BaudRate = 115200;
 	uart_init1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -69,15 +61,10 @@ void GPIO_set(){
 	uart_init1.Init.Mode       = UART_MODE_TX_RX;
 	uart_init1.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
 	HAL_UART_Init(&uart_init1);
-	//HAL_UART_MspInit(&uart_init1);
-	//__HAL_UART_ENABLE_IT(&uart_init1, UART_IT_RXNE);
+	
 
 }
 
-void readInput(){
-	HAL_UART_Receive_IT(&uart_init1, (uint8_t *)user_m, 1);
-	HAL_UART_Receive_IT(&uart_init6, (uint8_t *)user_s, 1);
-}
 
 void USART1_IRQHandler(){
 	HAL_UART_IRQHandler(&uart_init1);
@@ -87,56 +74,29 @@ void USART6_IRQHandler(){
 	HAL_UART_IRQHandler(&uart_init6);
 }
 
-void print_message(){
-	if(user_s[0] != 0){
-		HAL_UART_Transmit_IT(&uart_init1, (uint8_t*) user_s,1);
-		HAL_UART_Transmit_IT(&uart_init6, (uint8_t *)user_s, 1);
-		}
-		if(user_m[0] != 0){
-		HAL_UART_Transmit_IT(&uart_init1, (uint8_t*) user_m,1);
-		HAL_UART_Transmit_IT(&uart_init6, (uint8_t *)user_m, 1);
-		}
-		//HAL_Delay(100);
-		if(user_s[0] == 27 | user_m[0] == 27 ){
-		    HAL_UART_Transmit_IT(&uart_init1, (uint8_t*) halt_info, strlen(halt_info));
-		    HAL_UART_Transmit_IT(&uart_init6,  (uint8_t *)user_m, 1);
-		    return 0;
-		}
-		user_s[0] = 0;
-		user_m[0] = 0;
-}
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *uart_init){
-	if(uart_init->Instance == USART6){ //Nucleo
+	if(uart_init->Instance == USART6){ //Nucleo data
 			HAL_UART_Receive_IT(&uart_init6, (uint8_t *)user_s, 1);
 		}
-		if(uart_init->Instance == USART1){ //STM32
+		if(uart_init->Instance == USART1){ //STM32 data
 			HAL_UART_Receive_IT(&uart_init1, (uint8_t *)user_m, 1);
 		}
-	//printf("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii\r\n");
-	UartReady = 1;
-	//printf("userm %c; users %c", user_m[0],user_s[0]);
-	if(user_s[0] != 0){
+	
+	if(user_s[0] != 0){//Determine if data is from Nucleo board
 		HAL_UART_Transmit(&uart_init1, (uint8_t*) user_s,1,10);
 		HAL_UART_Transmit(&uart_init6, (uint8_t*)user_s, 1,10);
 	}
-	if(user_m[0] != 0){
+	if(user_m[0] != 0){//Determine ifndata is from DISCO board
 		HAL_UART_Transmit(&uart_init1, (uint8_t*) user_m,1,10);
 		HAL_UART_Transmit(&uart_init6, (uint8_t*)user_m, 1,10);
 	}
-		//HAL_Delay(100);
-	if(user_s[0] == 27 | user_m[0] == 27 ){
+	
+	if(user_s[0] == 27 | user_m[0] == 27 ){// Determine if the received information is ESC
 		HAL_UART_Transmit(&uart_init1, (uint8_t*) halt_info, strlen(halt_info),10);
 		HAL_UART_Transmit(&uart_init6,  (uint8_t *)user_m, 1,10);
 		stop =1;//If stop ==1 program halt
-		   // return 0;
     }
 	user_s[0] = 0;
 	user_m[0] = 0;
-//	if(uart_init->Instance == USART6){
-//		HAL_UART_Receive_IT(&uart_init6, (uint8_t *)user_s, 1);
-//	}
-//	if(uart_init->Instance == USART1){
-//		HAL_UART_Receive_IT(&uart_init1, (uint8_t *)user_m, 1);
-//	}
 }
