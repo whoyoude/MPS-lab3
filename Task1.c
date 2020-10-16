3eac6eb613228f154b8b45d79033e9cef07d27fd
whoyoude <62922857+whoyoude@users.noreply.github.com>
2020-10-15 23:32:30 -0400

    Update Task1.c

--- Task1.c
+++ Task1.c
@@ -19,19 +19,20 @@ int main(void) {
 
 	while(1){
 		readInput();
-		if(user_s[0] != 0){
+		if(user_s[0] != 0){//Determine if signal is from Nucleo board
 		HAL_UART_Transmit(&uart_init1, (uint8_t*) user_s,1, 10);
 		HAL_UART_Transmit(&uart_init6, (uint8_t *)user_s, 1, 10);
 		}
-		if(user_m[0] != 0){
+		if(user_m[0] != 0){//Determine if signal is from DISCO board
 		HAL_UART_Transmit(&uart_init1, (uint8_t*) user_m,1, 10);
 		HAL_UART_Transmit(&uart_init6, (uint8_t *)user_m, 1, 10);
 		}
-		if(user_s[0] == 27 | user_m[0] == 27 ){
+		if(user_s[0] == 27 | user_m[0] == 27 ){//Determine if signal is ESC
 		    HAL_UART_Transmit(&uart_init1, (uint8_t*) halt_info, strlen(halt_info), 10);
 		    HAL_UART_Transmit(&uart_init6,  (uint8_t *)user_m, 1, 10);
 		    return 0;
 		}
+		//Clear both arrays
 		user_s[0] = 0;
 		user_m[0] = 0;
 	}
@@ -40,6 +41,7 @@ int main(void) {
 void GPIO_set(){
 	HAL_Init();
     SystemClock_Config();
+	//Initiate USART6
 	uart_init6.Instance = USART6;
 	uart_init6.Init.BaudRate = 38400;
 	uart_init6.Init.WordLength = UART_WORDLENGTH_8B;
@@ -49,7 +51,7 @@ void GPIO_set(){
 	uart_init6.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
 	HAL_UART_Init(&uart_init6);
 	HAL_UART_MspInit(&uart_init6);
-
+        //Initiate USART1
 	uart_init1.Instance = USART1;
 	uart_init1.Init.BaudRate = 115200;
 	uart_init1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -61,7 +63,7 @@ void GPIO_set(){
 	HAL_UART_MspInit(&uart_init1);
 }
 
-void readInput(){
+void readInput(){//Receive data from both USART1 and USART6
 	HAL_UART_Receive(&uart_init1, (uint8_t *)user_m, 1, 10);
 	HAL_UART_Receive(&uart_init6, (uint8_t *)user_s, 1, 10);
 }
